bc2b0becae6ec54a00c3a46f87b4d213a3cf94c4
vidwan2804 <141732792+vidwan2804@users.noreply.github.com>
2025-03-18 15:39:29 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,6 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+ cout<<"I am Vidwan";
   return 0;
 }
